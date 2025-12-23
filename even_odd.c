e7ce5636393d4fff6fd87f746d5acc438c62c3a6
arpita-padhy <arpita.padhyit24@ssipmt.com>
2025-12-23 19:41:36 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,16 @@
+//wap to print whether the number is even or odd//
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n/2==0)
+        printf("the number is even",n);
+    
+    else
+    
+         printf("the number is odd",n);
+    
+    return 0;
+}
---
+++ positive_negative.c
@@ -0,0 +1,17 @@
+//wap to print whether the number is negative or positive//
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("the number is positive");
+    }
+    else
+    {
+         printf("the number is negative");
+    }
+    return 0;
+}
